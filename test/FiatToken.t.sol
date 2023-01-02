fa7fc5be892a06de32acd4809527d34313ff0b7f
Igor Yalovoy <to@ylv.io>
2023-01-02 17:28:18 -0600

    Use proxy

--- test/FiatToken.t.sol
+++ test/FiatToken.t.sol
@@ -3,9 +3,11 @@ pragma solidity >=0.6.2 <=0.9.0;
 
 import "forge-std/Test.sol";
 import "../src/FiatToken.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract USDCTest is Test {
     FiatTokenV2_1 public token;
+    FiatTokenV2_1 public proxy;
 
     address payable internal alice = payable(makeAddr("alice"));
     address payable internal bob = payable(makeAddr("bob"));
@@ -23,44 +25,58 @@ contract USDCTest is Test {
             alice,
             alice
         );
+        proxy = FiatTokenV2_1(address(new ERC1967Proxy(
+            address(token),
+            abi.encodeWithSignature(
+                "initialize(string,string,string,uint8,address,address,address,address)",
+                "USD Coin",
+                "USDC",
+                "USD",
+                6,
+                alice,
+                alice,
+                alice,
+                alice
+            )
+        )));
 
         vm.label(alice, "alice");
         vm.label(bob, "bob");
         vm.label(bob, "frank");
 
         vm.prank(alice);
-        token.configureMinter(alice, 9000000);
+        proxy.configureMinter(alice, 9000000);
 
         vm.prank(alice);
-        token.mint(alice, 1000000);
+        proxy.mint(alice, 1000000);
 
         vm.prank(alice);
-        token.mint(frank, 1);
+        proxy.mint(frank, 1);
     }
 
     function testTransfer() public {
         vm.prank(alice);
 
-        token.transfer(bob, 1000000);
+        proxy.transfer(bob, 1000000);
 
-        assertEq(token.balanceOf(alice), 0);
-        assertEq(token.balanceOf(bob), 1000000);
+        assertEq(proxy.balanceOf(alice), 0);
+        assertEq(proxy.balanceOf(bob), 1000000);
     }
 
     function testTransferToNonEmptyAccount() public {
         vm.prank(alice);
-        token.transfer(frank, 1);
+        proxy.transfer(frank, 1);
 
-        assertEq(token.balanceOf(frank), 2);
+        assertEq(proxy.balanceOf(frank), 2);
     }
 
     function testTransferHot() public {
         vm.prank(alice);
-        token.transfer(frank, 1);
+        proxy.transfer(frank, 1);
 
         vm.prank(frank);
-        token.transfer(alice, 1);
+        proxy.transfer(alice, 1);
 
-        assertEq(token.balanceOf(frank), 1);
+        assertEq(proxy.balanceOf(frank), 1);
     }
 }
